6763404b86d48db6ea07fcbbcb36e9c48e365f51
brainbuddydev <brainbuddydev@gmail.com>
2025-01-09 12:11:15 +0700

    add animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { useAIStore } from "@/hook/ai-store";
+import gsap from "gsap";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
 import { useEffect, useRef, useState } from "react";
 import { ReactTyped } from "react-typed";
+import React from "react";
 
 const Home = () => {
   const { response, loading, error, fetchResponse } = useAIStore();
@@ -142,105 +145,151 @@ const Home = () => {
     }
   }, [response]);
 
-  return (
-    <div className="grainy-background min-h-screen flex flex-col text-white">
-      <div className="header p-[40px] flex items-center justify-between">
-        <h1 className="text-4xl">BrainBuddy</h1>
-        <div className="flex gap-4">
-          <h6>Twitter</h6>
-          <h6>Telegram</h6>
-          <h6>Dex Screener</h6>
-        </div>
-      </div>
-      <div className="home grow flex flex-col justify-center items-center">
-        <Image
-          className="absolute blur-[180px] bottom-[-93px] left-[14px]"
-          src="/blur-circle.png"
-          alt=""
-          width={618}
-          height={618}
-        />
-        <Image
-          className="absolute top-[134px] right-[376px]"
-          src="/star.png"
-          alt=""
-          width={80}
-          height={80}
-        />
-        <h2 className="text-8xl font-bold text-white text-center">
-          <ReactTyped strings={[answer]} typeSpeed={50} startDelay={1000} />
-        </h2>
-        <p className="m-0 p-0 my-6 font-light text-xl">
-          {firstTime
-            ? "What would you like to call me?"
-            : "Generate your own AI Buddy! Make your life easier with BrainBuddy"}
-        </p>
-        <form onSubmit={handleSubmit} className="w-1/2">
-          <div className="mt-6 flex flex-col gap-8 w-full">
-            <input
-              type="text"
-              placeholder={
-                firstTime ? "Please enter my name" : "What can I help you?"
-              }
-              className="px-8 py-6 rounded-full outline-none text-black"
-              value={firstTime ? aiName : command}
-              onChange={
-                firstTime
-                  ? (e) => setAiName(e.target.value)
-                  : (e) => setCommand(e.target.value)
-              }
-            />
+  useGSAP(() => {
 
-            <div className="flex justify-center gap-2">
-              <button
-                className="px-4 py-3 bg-black text-white rounded-full flex gap-2 items-center justify-between"
-                type="submit"
-                disabled={loading}
-              >
-                Submit
-                <Image src="/left-arrow.png" alt="" width={32} height={32} />
-              </button>
-              {!firstTime && (
-                <button
-                  type="button"
-                  className="px-4 py-3 bg-black text-white rounded-full flex gap-2 items-center justify-between"
-                  onClick={handleStartListening}
-                  disabled={loading}
-                >
-                  {isListening ? `${aiName} Listening` : "Start Talking"}
-                </button>
-              )}
-            </div>
-          </div>
-        </form>
-      </div>
-      <div className="about flex flex-col justify-center items-center">
-        <h1 className="highlighted-text-shadow">Who is BrainBuddy?</h1>
-        <div className="about-container">
-          <div className="about-text">
-            <div className="card3">
-              $BB is an AI Personal Assistant of the future. It’s more than just
-              a tool—it’s your digital companion, always ready to help you with
-              anything you need. From answering questions to managing tasks,
-              BrainBuddy is designed to think, adapt, and grow with you. With
-              BrainBuddy, the possibilities are endless. Customize its
-              personality, voice, and tone to create an assistant that feels
-              like a true reflection of you. As you interact, BrainBuddy learns
-              and evolves, becoming smarter and more intuitive with every
-              conversation. It’s not just about getting things done—it’s about
-              experiencing a seamless, intelligent connection that understands
-              you like no other. With BrainBuddy, the future of AI is here, and
-              it’s personal, smart, and always by your side.
-            </div>
+    const radius = 400; // Radius of the orbit
+    const centerX = 20; // X center of the orbit
+    const centerY = -100; // Y center of the orbit
+
+
+    gsap.to("#circle", {
+      rotation: 360, // Rotate the circle 360 degrees
+      duration: 10, // Time for one full rotation
+      repeat: -1, // Infinite looping
+      ease: "linear", // Smooth and continuous rotation
+    });
+
+    gsap.to("#blur-circle", {
+      duration: 10,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: function () {
+        const progress: string = gsap.getProperty(this, "progress").toString(); // Progress from 0 to 1
+        const angle = parseFloat(progress) * Math.PI * 2; // Convert progress to radians
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+
+        gsap.set("#blur-circle", { x, y });
+      },
+    });
+  }, []);
+
+  return (
+    <div className="grainy-background">
+      <div className="min-h-screen flex flex-col text-white max-w-[1200px] my-0 mx-auto">
+        <div className="header p-[40px] flex items-center justify-between">
+          <h1 className="text-4xl">BrainBuddy</h1>
+          <div className="flex gap-4">
+            <h6>Twitter</h6>
+            <h6>Telegram</h6>
+            <h6>Dex Screener</h6>
           </div>
         </div>
-        <div className="ca-address">
-          <button className="shiny-cta">
-            <span>CA : 9geNBCEuk1iUqMM9ru23a8yhNVCVkN5BN5NNvrdjpump</span>
-          </button>
+        <div className="home grow flex flex-row justify-center items-center">
+          <img
+            className="absolute bottom-[9%] left-[12%]"
+            id="circle"
+            src="/orbit-circle.png"
+          />
+          <Image
+            className="absolute blur-[60px] bottom-[-93px] left-[14px]"
+            src="/green-circle.png"
+            id="blur-circle"
+            alt=""
+            width={618}
+            height={618}
+          />
+          <Image
+            className="absolute top-[134px] right-[376px]"
+            src="/star.png"
+            alt=""
+            width={80}
+            height={80}
+          />
+
+          <div className="flex basis-1/2">
+            <img className="absolute top-[25%] left-[14%]" src="/robot-head.png" style={{width: '35%'}}  />
+          </div>
+
+          <div className="flex basis-1/2 flex-col items-center justify-center">
+            <h2 className="text-8xl font-bold text-white text-center">
+              <ReactTyped strings={[answer]} typeSpeed={50} startDelay={1000} />
+            </h2>
+            <p className="m-0 p-0 my-6 font-light text-xl">
+              {firstTime
+                ? "What would you like to call me?"
+                : "Generate your own AI Buddy! Make your life easier with BrainBuddy"}
+            </p>
+            <form onSubmit={handleSubmit} className="w-1/2">
+              <div className="mt-6 flex flex-col gap-8 w-full">
+                <input
+                  type="text"
+                  placeholder={
+                    firstTime ? "Please enter my name" : "What can I help you?"
+                  }
+                  className="px-8 py-6 rounded-full outline-none text-black"
+                  value={firstTime ? aiName : command}
+                  onChange={
+                    firstTime
+                      ? (e) => setAiName(e.target.value)
+                      : (e) => setCommand(e.target.value)
+                  }
+                />
+
+                <div className="flex justify-center gap-2">
+                  <button
+                    className="px-4 py-3 bg-black text-white rounded-full flex gap-2 items-center justify-between"
+                    type="submit"
+                    disabled={loading}
+                  >
+                    Submit
+                    <Image src="/left-arrow.png" alt="" width={32} height={32} />
+                  </button>
+                  {!firstTime && (
+                    <button
+                      type="button"
+                      className="px-4 py-3 bg-black text-white rounded-full flex gap-2 items-center justify-between"
+                      onClick={handleStartListening}
+                      disabled={loading}
+                    >
+                      {isListening ? `${aiName} Listening` : "Start Talking"}
+                    </button>
+                  )}
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+        <div className="about flex flex-col justify-center items-center">
+          <h1 className="highlighted-text-shadow">Who is BrainBuddy?</h1>
+          <div className="about-container">
+            <div className="about-text">
+              <div className="card3">
+                $BB is an AI Personal Assistant of the future. It’s more than just
+                a tool—it’s your digital companion, always ready to help you with
+                anything you need. From answering questions to managing tasks,
+                BrainBuddy is designed to think, adapt, and grow with you. With
+                BrainBuddy, the possibilities are endless. Customize its
+                personality, voice, and tone to create an assistant that feels
+                like a true reflection of you. As you interact, BrainBuddy learns
+                and evolves, becoming smarter and more intuitive with every
+                conversation. It’s not just about getting things done—it’s about
+                experiencing a seamless, intelligent connection that understands
+                you like no other. With BrainBuddy, the future of AI is here, and
+                it’s personal, smart, and always by your side.
+              </div>
+            </div>
+          </div>
+          <div className="ca-address">
+            <button className="shiny-cta">
+              <span>CA : 9geNBCEuk1iUqMM9ru23a8yhNVCVkN5BN5NNvrdjpump</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
